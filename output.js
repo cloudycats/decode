33c2ba6aeae93ca30fe0a925fb5059e75b6b7c45
action <action@github.com>
2025-10-18 01:27:22 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,4 +1,4 @@
-//Sat Oct 18 2025 01:26:50 GMT+0000 (Coordinated Universal Time)
+//Sat Oct 18 2025 01:27:22 GMT+0000 (Coordinated Universal Time)
 //Base:<url id="cv1cref6o68qmpt26ol0" type="url" status="parsed" title="GitHub - echo094/decode-js: JS混淆代码的AST分析工具 AST analysis tool for obfuscated JS code" wc="2165">https://github.com/echo094/decode-js</url>
 //Modify:<url id="cv1cref6o68qmpt26olg" type="url" status="parsed" title="GitHub - smallfawn/decode_action: 世界上本来不存在加密，加密的人多了，也便成就了解密" wc="741">https://github.com/smallfawn/decode_action</url>
 const Illilii1 = "https://raw.githubusercontent.com/Alex0510/Eric/master/surge/Script/evn.js";
